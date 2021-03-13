faf57dafa9ac1cf8951217f1f7d74547bec59126
Hyungryul <hyungryul@Hyungryului-MacBookAir.local>
2021-03-13 15:27:18 +0900

    setting for react apollo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
+import { ApolloProvider } from "react-apollo-hooks";
+import Client from "../Apollo/Client";
 import GlobalStyles from "../Styles/GlobalStyles";
 import AppRouter from "./Router";
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={Client}>
       <GlobalStyles />
       <AppRouter isLoggedIn={!false} />
-    </>
+    </ApolloProvider>
   );
 }
 
