9612ee2cb5a112d4a5cf586884356c990c5ed05a
Hyungryul <hyungryul@Hyungryului-MacBookAir.local>
2021-03-16 21:43:15 +0900

    local login success

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -1,25 +1,25 @@
 import React, { useEffect } from "react";
-import { KAKAO_LOGIN } from "./AuthQuery";
+import { KAKAO_LOGIN, LOCAL_LOG_IN } from "./AuthQuery";
 import { useMutation } from "react-apollo-hooks";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   const [kakaoLogin, { data }] = useMutation(KAKAO_LOGIN);
+  const [localLogInMutation] = useMutation(LOCAL_LOG_IN);
 
   useEffect(() => {
     if (data) {
-      console.log(data);
+      localLogInMutation({ variables: { token: data.socialAuth.token } });
     }
-  }, [data]);
+  });
 
   // 카카오 로그인 버튼을 생성합니다.
-  const loginWithKakao = (e) => {
+  const loginWithKakao = () => {
     window.Kakao.Auth.login({
       success: function (authObj) {
         kakaoLogin({
           variables: { provider: "kakao", accessToken: authObj.access_token },
         });
-        console.log(authObj.access_token);
       },
       fail: function (err) {
         alert(JSON.stringify(err));
